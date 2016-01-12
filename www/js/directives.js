8957d55a9df22ef58580e79def1286acffa365d3
LocNT <locnt@vn.ibm.com>
2016-01-12 17:22:08 +0700

    add default img

--- www/js/directives.js
+++ www/js/directives.js
@@ -3,12 +3,22 @@
  */
 angular.module('starter.directives', [])
   .directive('errSrc', function($rootScope, ImageService) {
-  return {
-    link: function(scope, element, attrs) {
-      element.bind('error', function() {
-        console.log("removeDownloadItem " + attrs.errSrc);
-        ImageService.removeDownloadItem(attrs.errSrc);
+    return {
+      link: function(scope, element, attrs) {
+        element.bind('error', function() {
+          console.log("removeDownloadItem " + attrs.errSrc);
+          ImageService.removeDownloadItem(attrs.errSrc);
+        });
+      }
+    }
+  })
+.directive('fallbackSrc', function () {
+  var fallbackSrc = {
+    link: function postLink(scope, iElement, iAttrs) {
+      iElement.bind('load', function() {
+          angular.element(this).attr("src", iAttrs.fallbackSrc);
       });
     }
   }
-});
+  return fallbackSrc;
+});;
